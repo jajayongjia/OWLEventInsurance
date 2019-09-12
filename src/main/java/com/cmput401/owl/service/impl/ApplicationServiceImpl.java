140117a8a29ddea085ed0bb97b3954c0e0d7b5de
Apple <apple@ALEX.local>
2019-09-11 18:10:46 -0600

    add service

--- src/main/java/com/cmput401/owl/service/impl/ApplicationServiceImpl.java
+++ src/main/java/com/cmput401/owl/service/impl/ApplicationServiceImpl.java
@@ -1,7 +1,10 @@
 package com.cmput401.owl.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.cmput401.owl.service.ApplicationService;
 
+@Service("applicationService")
 public class ApplicationServiceImpl implements ApplicationService{
 
 }
